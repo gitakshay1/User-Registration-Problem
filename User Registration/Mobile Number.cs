a461791ec0f927bc6a947ff9012f3f0d3f014bc1
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 13:40:43 +0530

    [Akshay]-Phone Number

---
+++ User Registration/Mobile Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    internal class Mobile_Number
+    {
+        public void MobileNumber()
+        {
+            Console.WriteLine("Enter Mobile Number ");
+            var data=Console.ReadLine();
+            string pattern = "^[+][0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
+            Regex regex=new Regex(pattern);
+            if(regex.IsMatch(data))
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine(data + " is valid mobile number");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine(data+" is invaild mobile number");
+                Console.ResetColor();
+            }
+
+        }
+    }
+}
--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome User Regidtrsion \n1 Enter First Name \n2 Enter Last Name" +
-                "\n3 Enter Email Id");
+                "\n3 Enter Email Id \n4 Enter Mobile Number");
             int Choice=Convert.ToInt32(Console.ReadLine());
             switch(Choice)
             {
@@ -20,6 +20,10 @@
                 case 3:
                     Email_Id email_Id = new Email_Id();
                     email_Id.EmailId();
+                    break;
+                case 4:
+                    Mobile_Number mobile_Number = new Mobile_Number();
+                    mobile_Number.MobileNumber();
                     break;
                 default:
                     Console.WriteLine("Enter valid choice");
