a08b6737bc329e1a812049704282f3aad229a97d
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 07:57:55 +0530

    [Akshay]- First Name

---
+++ User Registration/First_Name.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    internal class First_Name
+    {
+        public void FirstName()
+        {
+            Console.WriteLine("Enter Firts Name");
+            var data = Console.ReadLine();
+            string pattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(data + " is a Valid Name");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(data + " is a Invalid Name");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ User Registration/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome User Regidtrsion \n1 To Enter First Name");
+            int Choice=Convert.ToInt32(Console.ReadLine());
+            switch(Choice)
+            {
+                case 1:
+                    First_Name first_Name=new First_Name();
+                    first_Name.FirstName();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid choice");
+                    break;
+            }
+        }
+    }
+}   
