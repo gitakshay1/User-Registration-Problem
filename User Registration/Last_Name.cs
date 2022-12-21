44412d72bbf3c4e296ff1700b6001d83da645cdc
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 09:36:39 +0530

    [Akshay]-Last Name

---
+++ User Registration/Last_Name.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    internal class Last_Name
+    {
+        public void Lastname()
+        {
+            Console.WriteLine("Enter Last Name");
+            var data = Console.ReadLine();
+            string pattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(data + " is a Valid Last Name");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(data + " is a Invalid Last Name");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome User Regidtrsion \n1 To Enter First Name");
+            Console.WriteLine("Welcome User Regidtrsion \n1 Enter First Name \n2 Enter Last Name");
             int Choice=Convert.ToInt32(Console.ReadLine());
             switch(Choice)
             {
                 case 1:
                     First_Name first_Name=new First_Name();
                     first_Name.FirstName();
+                    break;
+                case 2:
+                    Last_Name last_Name = new Last_Name();
+                    last_Name.Lastname();
                     break;
                 default:
                     Console.WriteLine("Enter valid choice");
